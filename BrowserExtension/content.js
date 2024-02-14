4d1fd5f20585f689554b824294def8975b679d2c
Datta Ban <datta.ban007@gmail.com>
2024-02-14 17:52:41 +0530

    Removed simple extension code

--- BrowserExtension/content.js
+++
@@ -1,9 +0,0 @@
-// content.js
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === 'saveContent') {
-    const content = document.documentElement.outerHTML;
-    chrome.storage.local.set({ 'savedContent': content }, function () {
-      console.log('Content saved.');
-    });
-  }
-});
